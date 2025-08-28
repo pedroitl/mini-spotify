f606343d0863cebd70fc03ce92d9ff0eb8d4bd2c
luiz lopes <neutagico@gmail.com>
2025-08-28 00:28:52 -0300

    melhorando a classe usuario e adicionando possiveis execoes nela tambem

--- src/model/Usuarios.java
+++ src/model/Usuarios.java
@@ -10,24 +10,47 @@ public class Usuarios {
     private List<Playlist> playlists;
 
     public Usuarios(String nome, String email) {
+        if (nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException("Nome nao pode ser vazio ou nulo!");
+        }
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("Nome nao pode ser vazio ou nulo!");
+        }
+
         this.nome = nome;
         this.email = email;
         this.playlists = new ArrayList<>();
     }
 
     public void criarPlaylist(String nomePlaylist) {
+        if (nomePlaylist == null || nomePlaylist.isBlank()) {
+            throw new IllegalArgumentException("Nome da playlist não pode ser nulo ou vazio");
+        }
+
+        for (Playlist p : playlists) {
+            if (p.getNome() != null && p.getNome().equalsIgnoreCase(nomePlaylist)) {
+                System.out.println("Já existe uma playlist com esse nome!");
+                return;
+            }
+        }
+
         Playlist nova = new Playlist(nomePlaylist, this);
         playlists.add(nova);
+        System.out.println("Playlist " + nomePlaylist + " Criada com sucesso!");
     }
 
     public void visualizarPlaylist() {
-        for (Playlist p : playlists) {
-            System.out.println(p.getNome());
+        if (playlists.isEmpty()) {
+            System.out.println("Nenhuma playlist encontrada!");
+        } else {
+            for (Playlist p : playlists) {
+                System.out.println(p.getNome());
+            }
         }
     }
 
-    public void removerPlaylist(String nomePlaylist){
-        playlists.removeIf(playlist -> playlist.getNome().equals(nomePlaylist));
+    public boolean removerPlaylist(String nomePlaylist) {
+        return playlists.removeIf(playlist -> playlist.getNome() != null && playlist.getNome().equals(nomePlaylist));
     }
 
     public String getNome() {
@@ -46,11 +69,5 @@ public class Usuarios {
         this.email = email;
     }
 
-    public List<Playlist> getPlaylists() {
-        return playlists;
-    }
 
-    public void setPlaylists(List<Playlist> playlists) {
-        this.playlists = playlists;
-    }
 }
