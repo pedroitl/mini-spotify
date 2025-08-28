6e2476dea43819c6121458472fe72525edd41fb4
Pedro Italo <pedroitl15@gmail.com>
2025-08-28 15:16:30 -0300

    Editando main

--- src/Sistema.java
+++ src/Sistema.java
@@ -9,27 +9,23 @@ public class Sistema {
     private List<Usuarios> usuarios;
     private Catalogo catalogo;
 
-    // Construtor
     public Sistema() {
         this.usuarios = new ArrayList<>();
         this.catalogo = new Catalogo();
     }
 
-    // Cadastrar usuário no sistema
     public void cadastrarUsuario(String nome, String email) {
         Usuarios usuario = new Usuarios(nome, email);
         usuarios.add(usuario);
         System.out.println("Usuário cadastrado com sucesso: " + nome);
     }
 
-    // Iniciar o sistema (simulação)
     public void iniciar() {
         System.out.println("Sistema iniciado!");
         System.out.println("Número de usuários cadastrados: " + usuarios.size());
         System.out.println("Quantidade de mídias no catálogo: " + catalogo.getQtdMidias());
     }
 
-    // Métodos auxiliares (se quiser acessar dados depois)
     public List<Usuarios> getUsuarios() {
         return usuarios;
     }
