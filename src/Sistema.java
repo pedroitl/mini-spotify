ec78afa7b24c6a2905aa8d19a00f20884ce477f9
Andrey Savinir N. Araujo <savinirandrey42@gmail.com>
2025-08-28 15:05:47 -0300

    Update Sistema.java

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,5 +1,40 @@
-public class Sistema {
-    public static void main(String[] args) {
+import model.Usuarios;
+import service.Catalogo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Sistema {
+    private List<Usuarios> usuarios;
+    private Catalogo catalogo;
+
+    // Construtor
+    public Sistema() {
+        this.usuarios = new ArrayList<>();
+        this.catalogo = new Catalogo();
+    }
+
+    // Cadastrar usuário no sistema
+    public void cadastrarUsuario(String nome, String email) {
+        Usuarios usuario = new Usuarios(nome, email);
+        usuarios.add(usuario);
+        System.out.println("Usuário cadastrado com sucesso: " + nome);
+    }
+
+    // Iniciar o sistema (simulação)
+    public void iniciar() {
+        System.out.println("Sistema iniciado!");
+        System.out.println("Número de usuários cadastrados: " + usuarios.size());
+        System.out.println("Quantidade de mídias no catálogo: " + catalogo.getQtdMidias());
+    }
+
+    // Métodos auxiliares (se quiser acessar dados depois)
+    public List<Usuarios> getUsuarios() {
+        return usuarios;
+    }
+
+    public Catalogo getCatalogo() {
+        return catalogo;
     }
 }
