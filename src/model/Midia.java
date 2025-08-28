ae31c76f73ddc32cc589416353bc7ef6a4771a59
Iann Maia <iannmaial@gmail.com>
2025-08-28 14:54:42 -0300

    feat(model): criar classe abstrata Midia com atributos e método reproduzir()

--- src/model/Midia.java
+++ src/model/Midia.java
@@ -1,5 +1,39 @@
 package model;
 
-public abstract class Midia {
+import enums.Genero;
 
+public abstract class Midia {
+    protected String titulo;
+    protected String artista;
+    protected Genero genero;
+    protected Integer duracao;
+
+    public Midia(String titulo, String artista, Genero genero, Integer duracao) {
+        this.titulo = titulo;
+        this.artista = artista;
+        this.genero = genero;
+        this.duracao = duracao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public Integer getDuracao() {
+        return duracao;
+    }
+
+    public void exibirInfo() {
+        System.out.println(titulo +" - " +artista+" | " + "\n" + duracao + " " + genero);
+    }
+
+    public abstract void reproduzir();
 }
